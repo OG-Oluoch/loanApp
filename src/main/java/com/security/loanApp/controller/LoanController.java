84643458f6aba40fec2f3095fece53ffb68d4ff1
OG-Oluoch <makopiyo45@gmail.com>
2025-01-24 12:31:07 +0300

    fixed CORS issue

--- src/main/java/com/security/loanApp/controller/LoanController.java
+++ src/main/java/com/security/loanApp/controller/LoanController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/loans")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 public class LoanController {
 
     @Autowired
